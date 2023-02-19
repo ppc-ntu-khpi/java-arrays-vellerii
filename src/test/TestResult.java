68d94dbba6b0cc3e53262b3c2ece885265adbbd7
Valeriia Krychevska <90756841+vellerii@users.noreply.github.com>
2023-02-19 13:31:13 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,8 +5,8 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int[] theArray = {110, 24, 36, 453, 52, 340};
+        
+        Exercise.Calculate(theArray);
     }
 }
